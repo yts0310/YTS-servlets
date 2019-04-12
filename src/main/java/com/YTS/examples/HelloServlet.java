a446d713177bac418f613e0a0fa6ad275f126e37
yts0310 <yts0310@github.com>
2019-04-12 14:51:47 +0900

    Servlet

---
+++ src/main/java/com/YTS/examples/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.YTS.examples;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 서블릿은 데이터 처리만 하고 페이지 출력은 JSP와 같은 뷰 기술로 넘긴다.
+ * 
+ * @author Jacob
+ */
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 데이터를 만든다.
+		String name = "윤태식";
+
+		// 데이터를 request에 태운다.
+		request.setAttribute("name", name);
+
+		// /WEB-INF/jsp/hello.jsp로 forward 한다.
+		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,
+				response);
+	}
+}
